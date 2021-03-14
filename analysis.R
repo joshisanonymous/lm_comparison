0cffd24d7a857d17fd9042755ec9a2f40e209c0c
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2021-03-14 12:27:45 -0400

    Added a quick test run to make sure the comparison works at all

--- analysis.R
+++ analysis.R
@@ -1,8 +1,20 @@
 library(philentropy)
 
-P <- c(2, 8, 3, 7, 6, 4, 1, 3, 8, 0, 0, 0, 0, 0, 0, 0, 0)
-Q <- c(0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 2, 8, 9, 1, 5, 2, 3)
-test <- rbind(P, Q)
-  
+dataDir <- "./data/"
+files <- list.files(dataDir)
+
+for(file in files) {
+  objectName <- substr(file, 1, nchar(file) - 4)
+  assign(objectName, read.csv(paste(dataDir, file, sep = "")))
+}
+
+WordBi <- merge(euroEn1stWordBi, euroEn2ndWordBi, by.x = 1, by.y = 1, all = TRUE)
+WordBi[is.na(WordBi)] <- 0
+
+WordBi <- merge(euroEnWordBi, euroFrWordBi, by.x = 1, by.y = 1, all = TRUE)
+WordBi[is.na(WordBi)] <- 0
+
+test <- rbind(WordBi[,2], WordBi[,3])
+
 KL(test, est.prob = "empirical")
-cosine_dist(P, Q, FALSE)
+cosine_dist(test[1,], test[2,], FALSE)
