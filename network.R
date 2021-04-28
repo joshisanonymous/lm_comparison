6580ac04f45061cfd26cbc37aac322d56aba02b6
joshisanonymous <37247758+joshisanonymous@users.noreply.github.com>
2021-04-28 19:46:07 -0400

    Split dataset for classifier training

--- network.R
+++ network.R
@@ -25,8 +25,15 @@ tweets <- merge(tweets, communities, by = "screen_name")
 tweetsAnon <- subset(tweets, select = c(-screen_name, -reply_to_screen_name, -mentions_screen_name))
 tweetsAnon$text <- gsub("@[A-z0-9_]+[^A-z0-9_]", "", tweetsAnon$text)
 
+# Subset training and test data for topic classification
+picked <- sample(seq_len(nrow(tweetsAnon)), size = floor(0.01 * nrow(tweetsAnon)))
+tweetsAnonTrain <- tweetsAnon[picked,]
+tweetsAnonTest <- tweetsAnon[-picked,]
+
 # Output text files
 write.csv(tweetsAnon, file = "data/corpora/tweetsAnon.csv", fileEncoding = "UTF-8")
+write.csv(tweetsAnonTrain, file = "data/corpora/tweetsAnonTrain.csv", fileEncoding = "UTF-8")
+write.csv(tweetsAnonTest, file = "data/corpora/tweetsAnonTest.csv", fileEncoding = "UTF-8")
 
 # Cleanup
 rm(list = ls())
